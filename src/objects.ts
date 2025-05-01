5ad7be5ba52ed97e7cee09c3d4cc7bf692daaf6d
Haidar <yellowtape47@gmail.com>
2025-05-01 17:12:07 +0300

    Done

--- src/objects.ts
+++ src/objects.ts
@@ -27,8 +27,13 @@ function createBook(
   genre: string
 ): Book {
   // write your code here...
-
-  return {} as Book; // replace "{} as Book" with what you see is fit
+  const myBooks = {
+    title: title,
+    author: author,
+    publishedYear: publishedYear,
+    genre: genre,
+  };
+  return myBooks as Book; // replace "{} as Book" with what you see is fit
 }
 
 // DO NOT CHANGE THE LINE OF CODE BELOW (you can use it for testing your code)
@@ -50,8 +55,8 @@ const book = createBook(
  */
 function printBookTitleAndYear(book: Book): string {
   // write your code here...
-
-  return ""; // replace empty string with what you see is fit
+  let bookTitle = `${book.title} ${book["publishedYear"]}`;
+  return bookTitle; // replace empty string with what you see is fit
 }
 
 /**
@@ -66,7 +71,7 @@ function printBookTitleAndYear(book: Book): string {
  */
 function addPageCount(book: Book, pageCount: number): Book {
   // write your code here...
-
+  book.pageCount = pageCount;
   return book;
 }
 
@@ -88,7 +93,7 @@ function addPageCount(book: Book, pageCount: number): Book {
  */
 function addISBN(book: Book, ISBN: string): Book {
   // write your code here...
-
+  book.ISBN = ISBN;
   return book;
 }
 
@@ -110,7 +115,7 @@ function addISBN(book: Book, ISBN: string): Book {
  */
 function updatePublishedYear(book: Book, newYear: number): Book {
   // write your code here...
-
+  book.publishedYear = newYear;
   return book;
 }
 
@@ -135,6 +140,11 @@ function updatePublishedYear(book: Book, newYear: number): Book {
  */
 function addSecondAuthor(book: Book, additionalAuthor: string): Book {
   // write your code here...
+  if (Array.isArray(book.author)) {
+    book.author.push(additionalAuthor);
+  } else {
+    book.author = [book.author, additionalAuthor];
+  }
 
   return book;
 }
